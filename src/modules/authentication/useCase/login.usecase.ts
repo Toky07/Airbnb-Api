54d934929d71499e261bf70ddce69061a8da2b1e
toky <rasoanaivotokynyaina@gmail.com>
2026-06-10 19:13:37 +0300

    Remove log

--- src/modules/authentication/useCase/login.usecase.ts
+++ src/modules/authentication/useCase/login.usecase.ts
@@ -5,8 +5,6 @@ export class LoginUseCase {
   constructor(@Inject(TOKEN_GENERATOR) private readonly tokenGenerator: TokenGenerator) {}
 
   async execute({ email, password }: { email: string, password: string }): Promise<string> {
-    console.log('email', email);
-    console.log('password', password);
     return this.tokenGenerator.generate({ email, password });
   }
 }
