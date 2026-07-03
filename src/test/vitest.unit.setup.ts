f6c363b6546f34cfd8522e6e0b8a2bbbd0303815
toky <rasoanaivotokynyaina@gmail.com>
2026-07-03 14:26:15 +0300

    Fix build error

--- src/test/vitest.unit.setup.ts
+++ src/test/vitest.unit.setup.ts
@@ -1,8 +1,8 @@
 import { vi } from 'vitest';
-import './command-bus.mock';
+import { commandBusExecuteMock } from './command-bus.mock';
 
 vi.mock('../shared/useCase/bus/bus', async () => {
-  const { commandBusExecuteMock } = await import('./command-bus.mock');
+  // const { commandBusExecuteMock } = await import('./command-bus.mock.ts');
   return {
     CommandBus: {
       execute: commandBusExecuteMock,
